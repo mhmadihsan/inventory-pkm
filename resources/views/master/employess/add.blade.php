7249bb35865970c32a59f59a795fcd7852fc15da
mhmadihsan <>
2022-09-29 21:12:02 +0800

    breadcume updated text

--- resources/views/master/employess/add.blade.php
+++ resources/views/master/employess/add.blade.php
@@ -9,9 +9,9 @@
         <div class="section-header">
             <h1>Table</h1>
             <div class="section-header-breadcrumb">
-                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                <div class="breadcrumb-item"><a href="#">Bootstrap Components</a></div>
-                <div class="breadcrumb-item">Table</div>
+                @foreach(Request::segments() as $seg)
+                    <div class="breadcrumb-item"><a href="#">{{$seg}}</a></div>
+                @endforeach
             </div>
         </div>
 
